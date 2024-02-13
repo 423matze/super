d478b0c07a4eacae38bc9f892b4c1c6bcd722cdc
Matze Lenz <matze@423.ch>
2024-02-13 14:32:26 +0100

    Update mal423.js
    Working code - menu and mode switch

--- mal423.js
+++ mal423.js
@@ -1,24 +1,73 @@
-const wrap = document.getElementsByClassName(
-  "notion-callout bg-brown-light border"
-);
-const nav = document.getElementsByClassName('super-navbar__item-list');
+const SELECTOR = 'code:not([super-embed-seen])';
 
-console.log(nav[0]);
-const mylinks = document.querySelectorAll('.highlighted-background.bg-brown');
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', afterDOMLoaded);
+} else {
+    afterDOMLoaded();
+};
 
-console.log("mylinks",mylinks);
+function afterDOMLoaded() {
+    setupEmbeds();
+    setTimeout(addToggle(), 1000)
+};
 
-mylinks.forEach((link) => {
-  let firstChild = link.firstElementChild;
-  firstChild.classList.remove("link");
-  firstChild.classList.add("super-navbar__item")
+function addToggle() {
   
-  let newLi = document.createElement("li");
-  newLi.append(firstChild);
+    console.log("init toggle");
   
-  console.log("neuer link", newLi);
+    const elm = document.getElementById('my-toggle');
   
-  nav[0].append(newLi);
-  
+    elm.addEventListener("click", (e) => {  let mode = html.className === "theme-light" ? "theme-dark" : "theme-light";
+      
+      html.className = mode;
+      
+      localStorage['color-preference'] = mode.replace('theme-','');
+      
+    });
+    
+};
+
+function clearBlock(el) {
+    const node = el.parentElement.parentElement;
+    node.innerHTML = '';
+    return node;
+}
+
+function setupEmbeds() {
+    document.querySelectorAll(SELECTOR).forEach((node) => {
+        node.setAttribute('super-embed-seen', 1);
+        if (node.innerText.startsWith('super-embed:')) {
+            const code = node.innerText.replace('super-embed:', '');
+            const parentNode = clearBlock(node);
+            parentNode.innerHTML = code;
+            
+            parentNode.querySelectorAll('script').forEach((script) => {
+                if (!script.src && script.innerText) {
+                    eval(script.innerText);
+                    script.remove();  // Removing the original inline script after evaluation
+                } else {
+                    const scr = document.createElement('script');
+                    Array.from(script.attributes).forEach(attr => {
+                        scr.setAttribute(attr.name, attr.value);
+                    });
+                    script.parentNode.insertBefore(scr, script.nextSibling); // Insert new script right after the original one
+                    script.remove(); // Remove the original script
+                }
+            });
+        }
+    });
+}
+
+var observer = new MutationObserver(function (mutations) {
+    if (document.querySelector(SELECTOR)) {
+        setupEmbeds();
+        addToggle()
+    }
 });
-wrap[0].remove();
+
+observer.observe(document, {
+    attributes: false,
+    childList: true,
+    characterData: false,
+    subtree: true
+});
